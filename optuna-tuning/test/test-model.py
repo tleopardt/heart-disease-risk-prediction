c92feb5dc3ca05671ab1d323112aef83fb988db3
Jonatan <jntnafrizal@gmail.com>
2026-06-06 12:52:51 +0700

    cleanup spacing

--- optuna-tuning/test/test-model.py
+++ optuna-tuning/test/test-model.py
@@ -20,7 +20,6 @@ def predict(input_dict: dict) -> dict:
       'risk_level': risk
   }
 
-
 examples = [
     {'age':63,'sex':1,'cp':3,'trestbps':145,'chol':233,'fbs':1,
      'restecg':0,'thalach':150,'exang':0,'oldpeak':2.3,'slope':0,
